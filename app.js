dd765d83bd2cb1ccd983d0cfee3f6904e04683a3
thomastam73 <thomastam73@gmail.com>
2021-01-02 22:28:24 +0800

    update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use('/cases', require('./routes/case.routes'));
 app.use('/users', require('./routes/user.routes'));
 app.use('/rules', require('./routes/rule.routes'));
 app.use('/areas', require('./routes/area.routes'));
+app.use('/institutions', require('./routes/institution.routes'));
 
 // handle status 500 exception response
 app.use(handleErrorResponse);
---
+++ routes/institution.routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const InstitutionModel = require('../models/institution.model');
+const Institution = require('../controllers/InstitutionController');
+const { authentication } = require('../middlewares');
+
+const institutionController = new Institution(InstitutionModel);
+
+router.get('/', authentication, (req, res, next) =>
+  institutionController.getAllEntities(req, res, next)
+);
+router.get('/group', authentication, (req, res, next) =>
+  institutionController.getInstitutionGroup(req, res, next)
+);
+router.get('/:id', (req, res, next) =>
+  institutionController.getEntityById(req, res, next)
+);
+router.post('/', authentication, (req, res, next) =>
+  institutionController.createEntity(req, res, next)
+);
+router.put('/:id', authentication, (req, res, next) =>
+  institutionController.updateEntityById(req, res, next)
+);
+router.delete('/:id', authentication, (req, res, next) =>
+  institutionController.deleteEntityById(req, res, next)
+);
+
+module.exports = router;
